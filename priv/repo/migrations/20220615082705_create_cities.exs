b40bfc63cf7868fb55d9d24ed6aaebbdc194c37d
alain <alainnambi@gmail.com>
2022-06-15 10:33:12 +0200

    Don't allow empty data to be inserted and Add unique_index for [city and state]

--- priv/repo/migrations/20220615082705_create_cities.exs
+++ priv/repo/migrations/20220615082705_create_cities.exs
@@ -3,12 +3,14 @@ defmodule ScrapingData.Repo.Migrations.CreateCities do
 
   def change do
     create table(:cities) do
-      add :city, :string
-      add :state, :string
-      add :latitude, :float
-      add :longitude, :float
+      add :city, :string, null: false
+      add :state, :string, null: false
+      add :latitude, :float, null: false
+      add :longitude, :float, null: false
 
       timestamps()
+
+      unique_index(:cities, [:city, :state])
     end
   end
 end
