6663529bcdc8e344e83fe9427028fa0ca2964bef
Alain Nambi <alainnambi@gmail.com>
2022-06-17 14:51:17 +0300

    Function: insert-data and delete-all-data

--- lib/scraping_data_web/live/scraping/scraping_live.ex
+++ lib/scraping_data_web/live/scraping/scraping_live.ex
@@ -2,13 +2,29 @@ defmodule ScrapingDataWeb.ScrapingLive do
   use ScrapingDataWeb, :live_view
 
   alias ScrapingData.Cities
+  alias ScrapingData.Scraper
 
   def mount(_session, _params, socket) do
     list_cities = Cities.list_cities()
 
     {:ok,
       socket
-      |> assign(list_cities: list_cities)
+      |> assign(
+          list_cities: list_cities
+        )
     }
   end
+
+  def handle_event("insert-data", %{}, socket) do
+    case Scraper.run() do
+      _ok -> {:noreply, socket |> assign(list_cities: Cities.list_cities)}
+    end
+  end
+
+  def handle_event("remove-all-data", %{}, socket) do
+    case Cities.delete_all_city do
+      {_number, _nil} -> {:noreply, socket |> assign(list_cities: Cities.list_cities)}
+    end
+  end
+
 end
